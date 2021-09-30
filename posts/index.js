00883a32b97e58e4f9af2d0e5244689fa5cc3fb0
Tobechi <tobechichukwuleta125@gmail.com>
2021-09-30 07:55:37 -0700

    Its too early

---
+++ posts/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const { randomBytes } = require('crypto')
+const cors = require('cors')
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const posts = {}
+
+app.get('/posts', (req, res) => {
+    res.send(posts)
+})
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex')
+    const { title } = req.body
+
+    posts[id] = {
+        id, title
+    }
+    res.status(201).json(posts[id])
+})
+
+const port = 4000
+app.listen(port, () => {
+    console.log('Listening on port 4000')
+})
